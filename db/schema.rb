b2254ce3f5538d56ecec2e1bcde9c7d60d3b227c
pedrokelvin <pedrokelvin2006@gmail.com>
2017-09-14 22:37:18 -0300

    Creating project model
    Signed-off-by: martinslucas <lucas465@hotmail.com>
    Signed-off-by: pedrokelvin <pedrokelvin2006@gmail.com>
    Signed-off-by: cantuariavc <cantuariavc@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919201542) do
+ActiveRecord::Schema.define(version: 20170914234858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20170919201542) do
     t.string "github"
     t.string "password_digest"
     t.string "string"
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
