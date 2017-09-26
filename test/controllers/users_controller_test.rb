7816208e269e1dca3b808c6029ece88568ec72f5
Matheus456 <matheus456456@gmail.com>
2017-09-26 15:33:15 -0300

    Adding controller tests
    Signed-off-by: Matheus456 <matheus456456@gmail.com>
    Signed-off-by: DanielOda <danieloda35@gmail.com>

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,25 +1,54 @@
 require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = User.create(name: 'daniel', email: 'daniel@gmail.com', password: '123456789', password_confirmation: '123456789', github: 'danieloda')
+    @user = User.create(name: 'Ronaldo', email: 'Ronaldofenomeno@gmail.com', password: '123456789', password_confirmation: '123456789', github: 'ronaldobola')
   end
 
-  test "should create user" do
-    post '/users', @user
+  test "should login in" do
+    post '/authenticate', params: {email: 'Ronaldofenomeno@gmail.com', password: '123456789'}
+    assert_response :success
   end
 
+   test "should create" do
+     assert_difference('User.count') do
+       post '/users', params: {
+          "user":{
+            "email":"robakasdddi@email.com",
+            "name":"Fulvvano",
+            "password":"123456789",
+            "password_confirmation":"123456789",
+            "github":"fulanao"
+          }
+        }
+        assert_response :success
+     end
+  end
 
+  test "should delete" do
+    @token = AuthenticateUser.call(@user.email, @user.password)
+    delete "/users/#{@user.id}", headers: {:Authorization => @token.result}
+    assert_response :success
+  end
 
+  test "should show user" do
+    @token = AuthenticateUser.call(@user.email, @user.password)
+    get "/users/#{@user.id}", headers: {:Authorization => @token.result}
+    assert_response :success
+  end
 
-
-
-
-
-
-
-
-
-
-
-
+  test "should update user" do
+    @token = AuthenticateUser.call(@user.email, @user.password)
+    patch "/users/#{@user.id}", params: {
+       "user":{
+         "email":"robakasdddi@email.com",
+         "name":"Fulvvano",
+         "password":"123456789",
+         "password_confirmation":"123456789",
+         "github":"fulanao"
+       }
+     }, headers: {:Authorization => @token.result}
+     @user.reload
+     puts @user.name
+    assert_response :success
+  end
 end
