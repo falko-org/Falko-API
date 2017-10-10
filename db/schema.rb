337ec014c54c606ebf7f330e1dd7cd0ed8a9f137
Matheus456 <matheus456456@gmail.com>
2017-10-10 12:08:55 -0300

    Solving conflict bugs

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171010131420) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
