eea73f5dbd76a5e0d8cb16741cd018702e8de10b
Matheus456 <matheus456456@gmail.com>
2017-10-10 11:22:27 -0300

    Adding attributes validations to Release model
    Signed-off-by: Matheus456 <matheus456456@gmail.com>
    Signed-off-by: MatheusRich <matheusrichardt@gmail.com>
    Signed-off-by: pedrokelvin <pedrokelvin2006@gmail.com>

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,2 +1,5 @@
 class Release < ApplicationRecord
+  validates :name, presence: true, length:{in: 2..80}
+  validates :description, length:{in: 5..256}
+  validates :amount_of_sprints, presence: true
 end
