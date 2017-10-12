0944a4cfa72c640257d57d3da67c8a2fd543f49e
cantuariavc <cantuariavc@gmail.com>
2017-10-11 21:53:06 -0300

    Adding project_id to sprint database and configuring routes to project, user and sprints
    Signed-off-by: Thalisson Barreto de Melo Silva <tallisonmelo@hotmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
 
-  resources :users
-  resources :projects
-  resources :sprints
-
-  get 'users/:id/projects' => 'projects#index'
-  post 'users/:id/projects' => 'projects#create'
+  resources :users do
+    resources :projects do
+      resources :sprints
+    end
+  end
 
 end
--- db/migrate/20171010025258_create_sprint.rb
+++ db/migrate/20171010025258_create_sprint.rb
@@ -3,6 +3,9 @@ class CreateSprint < ActiveRecord::Migration[5.1]
     create_table :sprints do |t|
       t.string :name
       t.text :description
+      t.string :project_id
+      t.date :start_date
+      t.date :end_date
 
       t.timestamps
     end
