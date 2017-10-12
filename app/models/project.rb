ea0b85e6033f5686cf4aae3db68c099b4f0d662e
Thalisson Barreto de Melo Silva <tallisonmelo@hotmail.com>
2017-10-11 21:50:32 -0300

    Adding validations on project, user and sprints database
    Signed-off-by: Thalisson Barreto de Melo Silva <tallisonmelo@hotmail.com>
    Signed-off-by: cantuariavc <cantuariavc@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ApplicationRecord
   has_many :releases, dependent: :destroy
   belongs_to :user
 
+  belongs_to :user, dependent: :destroy
+  has_many :sprints
   validates :name, presence: true, length: { maximum: 128, minimum: 2 }
   validates :description, length: { maximum: 256 }
   validates :check_project, :inclusion => {:in => [true, false]}
