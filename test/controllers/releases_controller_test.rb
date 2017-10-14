750c74a6100acbcf95d01a68cca61e723ef4e874
pedrokelvin <pedrokelvin2006@gmail.com>
2017-10-14 09:32:59 -0300

    Refactoring test names
    Signed-off-by: Matheus456 <matheus456456@gmail.com>
    Signed-off-by: MatheusRich <matheusrichardt@gmail.com>
    Signed-off-by: alaxalves <alaxallves@gmail.com>

--- test/controllers/releases_controller_test.rb
+++ test/controllers/releases_controller_test.rb
@@ -13,15 +13,15 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
                         amount_of_sprints: "20", project_id: @project.id)
     @token = AuthenticateUser.call(@user.email, @user.password)
 
-    @user_another = User.create(name: 'Ronaldo', email: 'ronaldo@email.com',
+    @another_user = User.create(name: 'Ronaldo', email: 'ronaldo@email.com',
                         password: '123123', password_confirmation: '123123',
                         github: 'ronaldoGit')
-    @project_another = Project.create(name: "Futebol", description: "Description.",
+    @another_project = Project.create(name: "Futebol", description: "Description.",
                               user_id: @user.id)
-    @release_another = Release.create(name: "Real Madrid", description: "Descriptions",
+    @another_release = Release.create(name: "Real Madrid", description: "Descriptions",
                         initial_date: "2018-01-01", final_date: "2019-01-01",
                         amount_of_sprints: "20", project_id: @project.id)
-    @token_another = AuthenticateUser.call(@user_another.email, @user_another.password)
+    @another_token = AuthenticateUser.call(@another_user.email, @another_user.password)
 
   end
 
@@ -40,17 +40,18 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not create release without correct params" do
+    # Final date before initial date
     post "/projects/#{@project.id}/releases", params: {
       "release": {
         "name":"Release 01",
         "description":"First Release",
         "amount_of_sprints":"20",
         "initial_date":"2018-01-01",
-        "final_date":"2017-01-01"
+        "final_date":"1900-01-01"
       }
     }, headers: {:Authorization => @token.result}
 
-    assert_response 422
+    assert_response :unprocessable_entity
   end
 
 
@@ -65,13 +66,12 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
       }
     }
 
-    assert_response 401
+    assert_response :unauthorized
   end
 
-
-  test "should not get releases index" do
+  test "should not get releases index without authentication" do
     get "/projects/#{@project.id}/releases"
-    assert_response 401
+    assert_response :unauthorized
   end
 
   test "should get releases index" do
@@ -79,9 +79,9 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should not get releases show" do
+  test "should not get releases show without authentication" do
     get "/releases/#{@release.id}"
-    assert_response 401
+    assert_response :unauthorized
   end
 
   test "should get releases show" do
@@ -94,8 +94,15 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
     @old_description_release = @release.description
     @old_initial_date_release = @release.initial_date
 
-    patch "/releases/#{@release.id}", params:{ release: {name: "Daniboy", description: "CBlacku", initial_date: "2010-05-06"}}, headers: {:Authorization => @token.result}
+    patch "/releases/#{@release.id}", params:{
+      release: {
+        name: "Daniboy", description: "CBlacku", initial_date: "2010-05-06"
+      }
+    }, headers: {:Authorization => @token.result}
+
     @release.reload
+
+    assert_response :ok
     assert_not_equal @old_name_release, @release.name
     assert_not_equal @old_description_release, @release.description
     assert_not_equal @old_initial_date_release, @release.initial_date
@@ -106,8 +113,15 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
     @old_description_release = @release.description
     @old_initial_date_release = @release.initial_date
 
-    patch "/releases/#{@release.id}", params:{ release: {name: "Daniboy", description: "CBlacku", initial_date: "2010-05-06"}}
+    patch "/releases/#{@release.id}", params:{
+      release: {
+        name: "Daniboy", description: "CBlacku", initial_date: "2010-05-06"
+      }
+    }
+
     @release.reload
+
+    assert_response :unauthorized
     assert_equal @old_name_release, @release.name
     assert_equal @old_description_release, @release.description
     assert_equal @old_initial_date_release, @release.initial_date
@@ -118,8 +132,15 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
     @old_description_release = @release.description
     @old_initial_date_release = @release.initial_date
 
-    patch "/releases/#{@release.id}", params:{ release: {name: "", description: "", initial_date: ""}}, headers: {:Authorization => @token.result}
+    patch "/releases/#{@release.id}", params:{
+      release: {
+        name: "", description: "", initial_date: ""
+      }
+    }, headers: {:Authorization => @token.result}
+
     @release.reload
+
+    assert_response :unprocessable_entity
     assert_equal @old_name_release, @release.name
     assert_equal @old_description_release, @release.description
     assert_equal @old_initial_date_release, @release.initial_date
@@ -129,17 +150,21 @@ class ReleasesControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Release.count', -1) do
       delete "/releases/#{@release.id}", headers: {:Authorization => @token.result}
     end
-    assert_response 204
+
+    assert_response :no_content
   end
 
   test "should not destroy release without authentication" do
     assert_no_difference 'Release.count' do
         delete "/releases/#{@release.id}"
     end
+
+    assert_response :unauthorized
   end
 
-  test "should not destroy release with another user" do
-    delete "/releases/#{@release.id}", headers: {:Authorization => @token_another.result}
-    assert_response 401
+  test "should not destroy release of another user" do
+    delete "/releases/#{@release.id}", headers: {:Authorization => @another_token.result}
+
+    assert_response :unauthorized
   end
 end
