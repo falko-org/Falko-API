69218311a8f01650db7dd082ce6ab08261f71eb4
leossb36 <leossb36@gmail.com>
2017-10-19 23:57:42 -0200

    Creating Retrospective controller
    Signed-off-by: Matheus456 <matheus456456@gmail.com>
    Signed-off-by: alaxalves <alaxallves@gmail.com>

---
+++ app/controllers/retrospectives_controller.rb
@@ -0,0 +1,21 @@
+class RetrospectivesController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
