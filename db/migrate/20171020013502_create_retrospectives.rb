7102dd5cefab236d7a412144977b01004d11a287
MatheusRich <matheusrichardt@gmail.com>
2017-10-21 12:11:45 -0200

    Changing improvements, positive and negatives points to array

--- db/migrate/20171020013502_create_retrospectives.rb
+++ db/migrate/20171020013502_create_retrospectives.rb
@@ -2,9 +2,9 @@ class CreateRetrospectives < ActiveRecord::Migration[5.1]
   def change
     create_table :retrospectives do |t|
       t.text :sprint_report
-      t.text :positive_points
-      t.text :negative_points
-      t.text :improvements
+      t.text :positive_points, array: true, default: []
+      t.text :negative_points, array: true, default: []
+      t.text :improvements, array: true, default: []
 
       t.timestamps
     end
