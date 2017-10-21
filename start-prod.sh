7a5894ca5b46f56bfc5ee067816295687238b795
Álax Alves <alaxallves@gmail.com>
2017-10-20 23:06:58 -0200

    Updating the production environment

--- start-prod.sh
+++ start-prod.sh
@@ -2,18 +2,21 @@
 
 bundle check || bundle install
 
-if bundle exec rails db:exists; then
-  bundle exec rails db:migrate
-else
-  bundle exec rails db:create
-  bundle exec rails db:migrate
-fi
+while ! pg_isready -h falko-database -p 5432 -q -U postgres; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
-pidfile='/tmp/pids/server.pid'
+>&2 echo "Postgres is up - executing command"
+
+bundle exec rails db:create
+bundle exec rails db:migrate
+
+pidfile='/Falko-2017.2-BackEnd/tmp/pids/server.pid'
+
 if [ -f $pidfile ] ; then
 	echo 'Server PID file already exists. Removing it...'
 	rm $pidfile
 fi
 
-bundle exec rails s -p 3000 -b 0.0.0.0 
-
+bundle exec rails s -p 3000 -b 0.0.0.0
