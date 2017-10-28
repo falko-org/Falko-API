65037d7d6501cfea5d1eb2c91283a2e748f3810a
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-28 19:25:52 -0200

    Fixing Sprint route ralationship with Release
         Signed-off-by: MateusO97 <mat97_@hotmail.com>
         Signed-off-by: leossb36 <leossb36@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   resources :users, shallow: true do
     resources :projects do
       resources :releases do
+        resources :sprints
       end
-      resources :sprints
     end
   end
 end
