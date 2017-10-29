7457b91b5ad00dbb171999f80fa32dd337f5f182
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-29 02:08:14 -0200

    Fixing Story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,15 +5,8 @@ class Story < ApplicationRecord
   validates :description, length: { maximum: 256 }
   validates :assign, length: { maximum: 32, minimum: 2 }
   validates :pipeline, length: { maximum: 16, minimum: 4 }
-  validates :start_date, presence: true
+  validates :initial_date, presence: true
 
-  validate :is_final_date_valid?
-
-  private
-    def is_final_date_valid?
-      # It verifies if the dates exist and if end date is after start date.
-      if self[:start_date] && self[:conclusion_date] && self[:conclusion_date] < self[:start_date]
-        errors.add(:conclusion_date, "Start date must begins before end date!")
-      end
-    end
+  # Uncomment when DateValidationHelper PR is approved
+  # validate :is_final_date_valid?
 end
