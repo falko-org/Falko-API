d10c39327eed5a34fd62e27fde8be451eb101794
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-30 13:20:22 -0200

    Adding Stories migration
    Signed-off-by: MateusO97 <mat97_@hotmail.com>
    Signed-off-by: leossb36 <leossb36@gmail.com>

--- db/migrate/20171030145903_create_stories.rb
+++ db/migrate/20171030145903_create_stories.rb
@@ -1,10 +1,12 @@
-class CreateStory < ActiveRecord::Migration[5.1]
+class CreateStories < ActiveRecord::Migration[5.1]
   def change
     create_table :stories do |t|
       t.string :name
       t.text :description
       t.string :assign
       t.string :pipeline
+
+      t.timestamps
     end
   end
 end
