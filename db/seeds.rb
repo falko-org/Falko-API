529150a55a6a6bb64fa360216810234f2d876893
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-30 14:44:06 -0200

    Fixing Seeds
    Signed-off-by: MateusO97 <mat97_@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,61 +7,18 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Creating Users"
-users = User.create(
-  [
-    {
-      name: "Vitor Barbosa", email: "barbosa@gmail.com", password: "123456789",
-      password_confirmation: "123456789", github: "vituuGit"
-    },
-    {
-      name: "Carla", email: "carla@gmail.com", password: "123456789",
-      password_confirmation: "123456789", github: "carlaGit"
-    }
-  ]
-)
+users = User.create([{ name: "Vitor Barbosa", email: "barbosa@gmail.com", password: "123456789", password_confirmation: "123456789", github: "vituuGit" },
+                     { name: "Carla", email: "carla@gmail.com", password: "123456789", password_confirmation: "123456789", github: "carlaGit" }])
 
 puts "Creating Projects"
-projects = Project.create(
-  [
-    {
-      name: "Owla", description: "This project helps improving classes",
-      user_id: User.find_by(name: "Vitor Barbosa").id
-    },
-    {
-      name: "Falko", description: "Agile Projects Manager",
-      user_id: User.find_by(name: "Carla").id
-    }
-  ]
-)
-
-puts "Creating Sprints"
-releases = Sprint.create(
-  [
-    {
-      name: "Sprint 01", description: "First Sprint", initial_date: "01-08-2016",
-      final_date: "01-10-2016", project_id: Project.find_by(name: "Owla").id
-    },
-    {
-      name: "S1", description: "First Sprint", initial_date: "01-10-2016",
-      final_date: "01-12-2016", project_id: Project.find_by(name: "Falko").id
-    }
-  ]
-)
+projects = Project.create([{ name: "Owla", description: "This project helps improving classes", user_id: User.find_by(name: "Vitor Barbosa").id, check_project: true },
+                           { name: "Falko", description: "Agile Projects Manager", user_id: User.find_by(name: "Carla").id, check_project: true } ])
 
 puts "Creating Releases"
-releases = Release.create(
-  [
-    {
-      name: "R1", description: "RUP Release", initial_date: "01-08-2016",
-      final_date: "01-10-2016", project_id: Project.find_by(name: "Owla").id
-    },
-    {
-      name: "R2", description: "Agile Release", initial_date: "01-10-2016",
-      final_date: "01-12-2016", project_id: Project.find_by(name: "Owla").id
-    },
-    {
-      name: "R - 01", description: "First Release", initial_date: "01-08-2017",
-      final_date: "01-10-2016", project_id: Project.find_by(name: "Falko").id
-    }
-  ]
-)
+releases = Release.create([{ name: "R1", description: "RUP Release", initial_date: "01-08-2016", final_date: "01-10-2016", project_id: Project.find_by(name: "Owla").id },
+                           { name: "R2", description: "Agile Release", initial_date: "01-10-2016", final_date: "01-12-2016", project_id: Project.find_by(name: "Owla").id },
+                           { name: "R - 01", description: "First Release", initial_date: "01-08-2016", final_date: "01-10-2016", project_id: Project.find_by(name: "Falko").id }])
+
+puts "Creating Sprints"
+sprints = Sprint.create([{ name: "Sprint 01", description: "First Sprint", initial_date: "01-08-2016", final_date: "01-10-2016", release_id: Release.find_by(name: "R1").id },
+                         { name: "Sprint 2", description: "Second Sprint", initial_date: "01-10-2016", final_date: "01-12-2016", release_id: Release.find_by(name: "R2").id }])
