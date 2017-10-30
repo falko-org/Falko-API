45da349b14031d41493e6ee79bd6bf81a4e73183
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-30 13:20:22 -0200

    Adding Stories migration
    Signed-off-by: MateusO97 <mat97_@hotmail.com>
    Signed-off-by: leossb36 <leossb36@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030030854) do
+ActiveRecord::Schema.define(version: 20171030150130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 20171030030854) do
     t.index ["release_id"], name: "index_sprints_on_release_id"
   end
 
+  create_table "stories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "assign"
+    t.string "pipeline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "sprint_id"
+    t.date "initial_date"
+    t.date "final_date"
+    t.index ["sprint_id"], name: "index_stories_on_sprint_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -61,4 +74,5 @@ ActiveRecord::Schema.define(version: 20171030030854) do
   add_foreign_key "projects", "users"
   add_foreign_key "releases", "projects"
   add_foreign_key "sprints", "releases"
+  add_foreign_key "stories", "sprints"
 end
