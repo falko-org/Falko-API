46885c064197c084b5b4160a0abc973b95a8ef0f
Vinícius Cantuária <cantuariavc@gmail.com>
2017-11-05 23:17:00 -0200

    Fixing model tests

--- test/models/retrospective_test.rb
+++ test/models/retrospective_test.rb
@@ -2,28 +2,62 @@ require 'test_helper'
 
 class RetrospectiveTest < ActiveSupport::TestCase
   def setup
-    @user = User.create(name: 'Ronaldo', email: 'Ronaldofenomeno@gmail.com', password: '123456789', password_confirmation: '123456789', github: 'ronaldobola')
-    @project = Project.create(name: "Falko", description: "Esse projeto faz parte da disciplina MDS.", user_id: @user.id)
-    @sprint = Sprint.create(name: "Sprint1", description: "Essa sprint", project_id: @project.id, start_date: "23-04-1993", end_date: "23-04-2003")
-    @retrospective = Retrospective.create(sprint_report: "Sprint descricao", positive_points: ["Very good"], negative_points: ["No tests"], improvements: ["improve front-end"], sprint_id: @sprint.id)
+    @user = User.create(
+      name: 'Ronaldo',
+      email: 'ronaldofenomeno@gmail.com',
+      password: '123456789',
+      password_confirmation: '123456789',
+      github: 'ronaldobola'
+    )
+
+    @project = Project.create(
+      name: "Falko",
+      description: "Esse projeto faz parte da disciplina MDS.",
+      check_project: true,
+      user_id: @user.id
+    )
+
+    @release = Release.create(
+      name: "Release 1",
+      description: "First Release.",
+      initial_date: "01/01/2017",
+      final_date: "02/02/2019",
+      project_id: @project.id
+    )
+
+    @sprint = Sprint.create(
+      name: "Sprint1",
+      description: "Essa sprint",
+      initial_date: "23/04/1993",
+      final_date: "23/04/2003",
+      release_id: @release.id
+    )
+
+    @retrospective = Retrospective.create(
+      sprint_report: "Sprint description",
+      positive_points: ["Very good"],
+      negative_points: ["No tests"],
+      improvements: ["Improve front-end"],
+      sprint_id: @sprint.id
+    )
   end
 
   test "should save a retrospective" do
     assert @retrospective.save
   end
 
-  test "Restrospective should have sprint_report less than 1500" do
+  test "Restrospective should have sprint_report less than 1500 characters" do
     @retrospective.sprint_report = "a" * 1501
     assert_not @retrospective.save
   end
 
-  test "Restrospective should have sprint_report between 0 and 1500" do
-    @retrospective.sprint_report = ""
+  test "Restrospective should have sprint_report 600 characters" do
+    @retrospective.sprint_report = "a" * 600
     assert @retrospective.save
   end
 
-  test "Restrospective should have positive_points less than 500" do
-    @retrospective.positive_points = ["b" * 501]
+  test "Restrospective should have positive_points less than 500 characters" do
+    @retrospective.positive_points = "b" * 501
     assert_not @retrospective.save
   end
 
@@ -32,8 +66,8 @@ class RetrospectiveTest < ActiveSupport::TestCase
     assert_not @retrospective.save
   end
 
-  test "Restrospective should have negative_points less than 500" do
-    @retrospective.negative_points = ["c" * 501]
+  test "Restrospective should have negative_points less than 500 characters" do
+    @retrospective.negative_points = "c" * 501
     assert_not @retrospective.save
   end
 
@@ -42,8 +76,8 @@ class RetrospectiveTest < ActiveSupport::TestCase
     assert_not @retrospective.save
   end
 
-  test "Restrospective should have improvements less than 500" do
-    @retrospective.improvements = ["d" * 501]
+  test "Restrospective should have improvements less than 500 characters" do
+    @retrospective.improvements = "d" * 501
     assert_not @retrospective.save
   end
 
