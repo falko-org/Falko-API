7e8779c038503510fb4ea1988efae248b5aeba06
Vinícius Cantuária <cantuariavc@gmail.com>
2017-11-05 23:14:34 -0200

    Adding Retrospective route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       resources :releases do
         resources :sprints do
           resources :stories
+          resources :retrospectives
         end
       end
     end
