68cf8eaf4e563650187b4853ebb46ac14b2bc247
Matheus Roberto <matheus456456@gmail.com>
2017-11-11 17:35:11 -0200

    Creating Burndown method in Sprints
    Signed-off-by: MateusO97 <mat97_@hotmail.com>
    Signed-off-by: matheussbernardo <matheussbernardo@gmail.com>

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,7 +1,7 @@
 class SprintsController < ApplicationController
   include ValidationsHelper
 
-  before_action :set_sprint, only: [:show, :update, :destroy]
+  before_action :set_sprint, only: [:show, :update, :destroy, :burndown]
 
   before_action only: [:index, :create] do
     validate_release(0, :release_id)
@@ -53,6 +53,38 @@ class SprintsController < ApplicationController
     update_amount_of_sprints
   end
 
+  def burndown
+    total_points = 0
+    burned_stories = {}
+    coordenates = []
+
+    for story in @sprint.stories
+      total_points += story.story_points
+      if(story.pipeline == "Done")
+        if(burned_stories[story.final_date] == nil)
+          burned_stories[story.final_date] = story.story_points
+        else
+          burned_stories[story.final_date] += story.story_points
+        end
+      end
+    end
+
+
+    range = (@sprint.initial_date .. @sprint.final_date)
+
+    range.each do |date|
+      if burned_stories[date] == nil
+        burned_stories[date] = total_points
+      else
+        total_points -= burned_stories[date]
+        burned_stories[date] = total_points
+      end
+      coordanate = {x: date, y: burned_stories[date]}
+      coordenates.push(coordanate)
+    end
+    burned_stories = burned_stories.sort_by {|key, value| key}
+    render json: coordenates
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_sprint
