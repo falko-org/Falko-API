9a0377ac2c898ab17e065bf1d2871350fdcc504d
Álax Alves <alaxallves@gmail.com>
2017-11-11 16:44:19 -0200

    Fixing travis to deploy to the correct branch

---
+++ travis-deploy.sh
@@ -0,0 +1,10 @@
+# !/bin/sh
+# Pushing updated scripts only if its devel branch 
+
+if [[ "$TRAVIS_BRANCH" != "devel" ]]; then
+  echo "We're not on the devel branch."  
+else 
+  echo "YAY!!! We're  able to deploy."
+fi
+
+exit 0
