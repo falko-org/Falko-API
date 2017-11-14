0e76286cba77c490ddfe0dce47baf33abe9fa461
drianne <driah_as@hotmail.com>
2017-11-14 02:47:37 -0200

    Removing GitHub token
    Signed-off-by: Vinícius Cantuária <cantuariavc@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "authenticate", to: "authentication#authenticate"
   post "request_github_token", to: "users#request_github_token"
+  post "remove_github_token", to: "users#remove_github_token"
 
   get "repos", to: "projects#github_projects_list"
   get "projects/:id/contributors", to: "projects#get_contributors"
