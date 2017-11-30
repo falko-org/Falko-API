6034dade4b7a7e2aab2a8a6efc4d7aec5866de24
Daniel Oda <danieloda35@gmail.com>
2017-11-30 00:41:15 -0200

    Adding feature_id column in Story database
    Signed-off-by: Daniel Oda <danieloda35@gmail.com>
    Signed-off-by: Vinícius Cantuária <cantuariavc@gmail.com>
    Signed-off-by: ThalissonMelo <talissonmelo@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128014447) do
+ActiveRecord::Schema.define(version: 20171130021815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20171128014447) do
     t.date "final_date"
     t.integer "story_points"
     t.string "issue_number"
+    t.integer "feature_id"
     t.index ["sprint_id"], name: "index_stories_on_sprint_id"
   end
 
