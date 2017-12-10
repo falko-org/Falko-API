418cdf30b22557d737e13efceb3c71d18400a811
Leonardo Dos Santos Silva Barreiros <leossb36@gmail.com>
2017-12-10 20:20:05 -0200

    Creating evm models test
    Signed-off-by: Leonardo Dos Santos Silva Barreiros <leossb36@gmail.com>

--- test/models/earned_value_management_test.rb
+++ test/models/earned_value_management_test.rb
@@ -1,7 +1,64 @@
 require 'test_helper'
 
 class EarnedValueManagementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 	def setup
+    @user = User.create(
+      name: "Ronaldo",
+      email: "Ronaldofenomeno@gmail.com",
+      password: "123456789",
+      password_confirmation: "123456789",
+      github: "ronaldobola"
+    )
+
+    @project = Project.create(
+      name: "Falko",
+      description: "Some project description.",
+      user_id: @user.id,
+      is_project_from_github: true,
+      is_scoring: false
+    )
+
+    @release = Release.create(
+      name: "Release 1",
+      description: "First Release.",
+      initial_date: "01/01/2017",
+      final_date: "02/02/2019",
+      project_id: @project.id
+    )
+    
+    @evm = EarnedValueManagement.create(
+     	budget_actual_cost: "20",
+     	planned_release_points: "20",
+			planned_sprints: "9",
+			release_id: @release.id
+    )
+  end
+
+  test "should save a valid evm" do
+  	assert @evm.save
+  end
+
+  test "should not save a evm without a release" do
+  	@evm.release_id = nil
+  	assert_not @evm.save
+  end
+
+  test "should not save evm with blank budget_actual_cost" do
+  	@evm.budget_actual_cost = ""
+  	assert_not @evm.save
+  end
+
+  test "should not save evm with blank planned_release_points" do
+  	@evm.planned_release_points = ""
+  	assert_not @evm.save
+  end
+
+  test "should not create evm with negative parameters" do
+  	@wrong_evm = EarnedValueManagement.create(
+  		budget_actual_cost: "-1",
+  		planned_release_points: "-1",
+  		planned_sprints: "30"		
+  	)
+  	assert_not @wrong_evm.save
+  end
 end
