fe9da1bfd8d5bbebdbb9f72738f43cbdee94dc2f
drianne <driah_as@hotmail.com>
2017-12-12 18:14:27 -0200

    Generalizing method
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>

--- app/helpers/issue_graphic_helper.rb
+++ app/helpers/issue_graphic_helper.rb
@@ -12,33 +12,22 @@ module IssueGraphicHelper
 
     issues.each do |issue|
       if issue.created_at.to_date <= actual_date && issue.created_at.to_date >= first_date
-        created_issues(issue, total_open_issues, actual_date)
-        closed_issues(issue, total_closed_issues, actual_date)
+        interval_issues(issue, total_open_issues, actual_date)
+        interval_issues(issue, total_closed_issues, actual_date)
       end
     end
     number_of_issues = { opened_issues: total_open_issues, closed_issues: total_closed_issues, months: dates }
     return number_of_issues
   end
 
-  def created_issues (issue, total_open_issues, actual_date)
+  def interval_issues (issue, total_open_issues, actual_date)
+    last_month = actual_date - 1.month
     if issue.created_at.to_date.month == actual_date.month
       increment_total(total_open_issues, 2)
-    elsif issue.created_at.to_date.month == (actual_date - 1.month).month
+    elsif issue.created_at.to_date.month == last_month.month
       increment_total(total_open_issues, 1)
     else
       increment_total(total_open_issues, 0)
-    end
-  end
-
-  def closed_issues (issue, total_closed_issues, actual_date)
-    if issue.closed_at != nil
-      if issue.closed_at.to_date.month == actual_date.month
-        increment_total(total_closed_issues, 0)
-      elsif issue.closed_at.to_date.month == (actual_date - 1.month).month
-        increment_total(total_closed_issues, 1)
-      else
-        increment_total(total_closed_issues, 2)
-      end
     end
   end
 
