5698969549b68e6384d22990f52e6a22b3ce9b20
drianne <driah_as@hotmail.com>
2017-12-12 18:05:50 -0200

    Creating method increment_total
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>

--- app/helpers/issue_graphic_helper.rb
+++ app/helpers/issue_graphic_helper.rb
@@ -22,23 +22,27 @@ module IssueGraphicHelper
 
   def created_issues (issue, total_open_issues, actual_date)
     if issue.created_at.to_date.month == actual_date.month
-      total_open_issues[2] = total_open_issues[2] + 1
+      increment_total(total_open_issues, 2)
     elsif issue.created_at.to_date.month == (actual_date - 1.month).month
-      total_open_issues[1] = total_open_issues[1] + 1
+      increment_total(total_open_issues, 1)
     else
-      total_open_issues[0] = total_open_issues[0] + 1
+      increment_total(total_open_issues, 0)
     end
   end
 
   def closed_issues (issue, total_closed_issues, actual_date)
     if issue.closed_at != nil
       if issue.closed_at.to_date.month == actual_date.month
-        total_closed_issues[0] = total_closed_issues[0] + 1
+        increment_total(total_closed_issues, 0)
       elsif issue.closed_at.to_date.month == (actual_date - 1.month).month
-        total_closed_issues[1] = total_closed_issues[1] + 1
+        increment_total(total_closed_issues, 1)
       else
-        total_closed_issues[2] = total_closed_issues[2] + 1
+        increment_total(total_closed_issues, 2)
       end
     end
+  end
+
+  def increment_total(total, position)
+    total[position] = total[position] + 1
   end
 end
