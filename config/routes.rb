d186161589d98984d812763c02c6874e41975a76
Vinícius Cantuária <cantuariavc@gmail.com>
2017-12-13 12:01:56 -0200

    Adding Strories controller tests to increase coverage

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   get "projects/:id/contributors", to: "projects#get_contributors"
   post "projects/:id/issues/assignees", to: "issues#update_assignees"
 
-  get "sprints/:id/doing_stories", to: "stories#doing_list"
-  get "sprints/:id/done_stories", to: "stories#done_list"
   get "sprints/:id/to_do_stories", to: "stories#to_do_list"
+  get "sprints/:id/done_stories", to: "stories#done_list"
+  get "sprints/:id/doing_stories", to: "stories#doing_list"
 
   get "projects/:id/issues", to: "issues#index"
   post "projects/:id/issues", to: "issues#create"
