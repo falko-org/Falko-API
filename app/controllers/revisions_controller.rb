af22e981af0779251bfbb7de8147e1a3fd01f2e3
filipe.barcelos <you@example.com>
2019-04-29 23:33:27 -0300

    Adds revisions detailed documentation

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -11,19 +11,83 @@ class RevisionsController < ApplicationController
     validate_sprint_dependencies(:id, "revision")
   end
 
+  def_param_group :revision do
+    param :done_report, Array, of: String, :desc => "Sprint revision's done report"
+    param :undone_report, Array, of: String, :desc => "Sprint revision's undone report"
+    param :created_at, Date, "Revision's time of creation", :allow_nil => false
+    param :updated_at, Date, "Revision's time of edition", :allow_nil => false
+    param :sprint_id, :number, "Id of sprint revised"
+  end
+
+  api :GET, "/sprints/:sprint_id/revisions", "Show revisions for a sprint"
+  error :code => 401, :desc => "Unauthorized"
+  error :code => 404, :desc => "Not Found"
+  error :code => 500, :desc => "Internal Server Error"
+  description "Show all revisions of a specific sprint"
+  returns :code => 200, :desc => "Ok" do
+    param_group :revision
+  end
+  example <<-EOS
+  {
+    "id": 1,
+    "done_report": [
+        "Story US11 was done."
+    ],
+    "undone_report": [
+        "Story US21 was not done."
+    ],
+    "created_at": "2019-04-11T15:42:35.826Z",
+    "updated_at": "2019-04-11T15:42:35.826Z",
+    "sprint_id": 1
+  }
+  EOS
   def index
     @revision = @sprint.revision
     render json: @revision
   end
 
+  api :POST, "/sprints/:sprint_id/revisions", "Create a revision"
+  error :code => 401, :desc => "Unauthorized"
+  error :code => 404, :desc => "Not Found"
+  error :code => 500, :desc => "Internal Server Error"
+  description 'Create revision for a specific sprint'
+  param_group :revision
   def create
     create_sprint_dependencies("revision", revision_params)
   end
 
+  api :GET, "/revisions/:id", "Show a revision"
+  error :code => 401, :desc => "Unauthorized"
+  error :code => 404, :desc => "Not Found"
+  error :code => 500, :desc => "Internal Server Error"
+  description "Show a revision of a specific sprint"
+  returns :code => 200, :desc => "Ok" do
+    param_group :revision
+  end
+  example <<-EOS
+  {
+    "id": 2,
+    "done_report": [
+        "Story US12 was done."
+    ],
+    "undone_report": [
+        "Story US22 was not done."
+    ],
+    "created_at": "2019-04-11T15:42:35.851Z",
+    "updated_at": "2019-04-11T15:42:35.851Z",
+    "sprint_id": 2
+  }
+  EOS
   def show
     render json: @revision
   end
 
+  api :PATCH, "/revisions/:id", "Update a revision"
+  error :code => 401, :desc => "Unauthorized"
+  error :code => 404, :desc => "Not Found"
+  error :code => 500, :desc => "Internal Server Error"
+  description 'Update revision for a specific sprint'
+  param_group :revision
   def update
     if @revision.update(revision_params)
       render json: @revision
@@ -32,6 +96,12 @@ class RevisionsController < ApplicationController
     end
   end
 
+  api :DELETE, "/revisions/:id", "Delete a revision"
+  error :code => 401, :desc => "Unauthorized"
+  error :code => 404, :desc => "Not Found"
+  error :code => 500, :desc => "Internal Server Error"
+  description 'Delete revision for a specific sprint'
+  param_group :revision
   def destroy
     @revision.destroy
   end
