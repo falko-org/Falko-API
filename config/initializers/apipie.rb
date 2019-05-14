14a7d7f9cf4026228277ce573283f181972b1c6f
FilipeKN4 <filipekn4@gmail.com>
2019-05-14 16:33:33 -0300

    Remove useless comment

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,6 +3,5 @@ Apipie.configure do |config|
   config.api_base_url            = ""
   config.doc_base_url            = "/apipie"
   config.translate               = false
-  # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
