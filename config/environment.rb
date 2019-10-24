cc74f2145efe63223de1cf7b418be0b5ef3c5f03
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-10-23 23:23:35 -0300

    Adding sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => '',
+  :domain => 'gmail.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
